303fa199b637a2f25bc3767be6a7703c8200ddd2
Jacob Williams <jawoba004@gmail.com>
2023-03-16 11:17:38 -0600

    Added node.js quote puller and Julia instructionab

--- js/index.js
+++ js/index.js
@@ -18,26 +18,35 @@
       }
   }
   const game = new Game();
-  
+
+  function fun3(stonk){
+    
+  }
   
   function fun1(){
     var ticker = document.getElementById("symbo").value;
+    var price = fun3(stonk);
+    
+    var sharePrice = require("share-price");
+    sharePrice.getSharePrice({ stockSymbol: "APPL" }).then(function(stockPrice) {
+    console.log(stockPrice);
+  }).catch((error) => {
+      console.log(error);
+  });
     var now = new Date();
     var Buy = "Bought";
     var Bought = Buy.fontcolor("green");
     
     var number = document.getElementById("quant").value;
-    document.getElementById("changer").innerHTML += (Bought + " " + number + " shares of " + ticker + " on " + now + "<br>");
+    document.getElementById("changer").innerHTML += (Bought + " " + stockPrice + " dollars of " + ticker + " on " + now + "<br>");
   }
   function fun2(){
     var ticker = document.getElementById("symbo").value;
     var now = new Date();
     var sell = "Sold";
     var sold = sell.fontcolor("red");
-    var number = document.getElementById("quant").value;
-    document.getElementById("changer").innerHTML += (sold + " " + number + " shares of " + ticker + " on " + now + "<br>");
-  }
 
-  
-  
+    var number = document.getElementById("quant").value;
+    document.getElementById("changer").innerHTML += (sold + " " + number + " dollars of " + ticker + " on " + now + "<br>");
+  }
   
