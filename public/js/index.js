d52725b59a12aaa4181cb1b33c3de984d4dc18ab
Jacob Williams <jawoba004@gmail.com>
2023-03-31 16:35:55 -0600

    My intially stage of buying/selling stocks!

--- public/js/index.js
+++ public/js/index.js
@@ -34,49 +34,63 @@ function fun3(ticker) {
 
 function fun1() {
   var ticker = document.getElementById("symbo").value;
- 
+  var now = new Date();
+  var sell = "Bought";
+  var sold = sell.fontcolor("green");
+  var number = document.getElementById("quant").value;
+  
+  fetch(`/api/stock/${ticker}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const containerEl = document.querySelector('#buy');
 
-  fetch(`https://query1.finance.yahoo.com/v11/finance/quoteSummary/${ticker}?modules=financialData`)
-    .then((httpResult) => httpResult.json())
-    .then((stockData) => {
-      const stockPrice = stockData.quoteSummary.result[0].financialData.currentPrice.raw;
+      containerEl.textContent = `Bought ${number*data.price} dollars of ${data.ticker} on ${now}`
+    });
 
-      var now = new Date();
-      var quantEl = document.getElementById("quant");
-      quantEl.style.color = "green"
-      document.getElementById("changer").innerHTML += ("Bought " + quantEl.value + " dollars of " + stockPrice + " on " + now + "<br>");
-    })
 }
 function fun2() {
   var ticker = document.getElementById("symbo").value;
   var now = new Date();
   var sell = "Sold";
   var sold = sell.fontcolor("red");
-  
-  fetch(`/api/stock/TSLA`)
+  var number = document.getElementById("quant").value;
+
+  fetch(`/api/stock/${ticker}`)
     .then((response) => response.json())
     .then((data) => {
-      const containerEl = document.querySelector('#changer');
-
-      containerEl.textContent += `Sold ${number*data.price} dollars of ${data.ticker} on ${now}`
+      const containerEl = document.querySelector('#sell');
+      
+      containerEl.textContent = `Sold ${number*data.price} dollars of ${data.ticker} on ${now}`
     });
-
-  var number = document.getElementById("quant").value;
-//   document.getElementById("changer").innerHTML += (sold + " " + number*price + " dollars of " + ticker + " on " + now + "<br>");
+  // document.getElementById("changer").innerHTML += (sold + " " + number*price + " dollars of " + ticker + " on " + now + "<br>");
  }
 
+//  const containerEl = document.querySelector('#changer');
+//  containerEl.insertAdjacentElement('afterend', '<p>This is a <span id="bigboy">green</span> word in a paragraph.</p>');
+//  const spanElement = document.querySelector('#bigboy');
+//  spanElement.style.color = 'green';
+// function displayQuote(data) {
+
+//   fetch(`/api/stock/TSLA`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const containerEl = document.querySelector('#changer');
+
+//       containerEl.textContent = `Stock: ${data.ticker}, Price: ${data.price}`
+//     });
+// }
 
 
-function displayQuote(data) {
+// var ticker = document.getElementById("symbo").value;
+ 
 
-  fetch(`/api/stock/TSLA`)
-    .then((response) => response.json())
-    .then((data) => {
-      const containerEl = document.querySelector('#changer');
+  // fetch(`https://query1.finance.yahoo.com/v11/finance/quoteSummary/${ticker}?modules=financialData`)
+  //   .then((httpResult) => httpResult.json())
+  //   .then((stockData) => {
+  //     const stockPrice = stockData.quoteSummary.result[0].financialData.currentPrice.raw;
 
-      containerEl.textContent = `Stock: ${data.ticker}, Price: ${data.price}`
-    });
-}
-
-
-
+  //     var now = new Date();
+  //     var quantEl = document.getElementById("quant");
+  //     quantEl.style.color = "green"
+  //     document.getElementById("changer").innerHTML += ("Bought " + quantEl.value + " dollars of " + stockPrice + " on " + now + "<br>");
+  //   })
